b34681e9bfea343d1d8ce679f5c33d155eec7b93
DanielAngul0 <daniel.andres.angulo.perez>
2024-10-02 20:13:23 -0500

    agregando metodo 'post' y creando 'middleware' para reconocer codigo 'json'

--- Metodos-HTTP-Server/server-metodos.js
+++ Metodos-HTTP-Server/server-metodos.js
@@ -1,8 +1,11 @@
-// importanto el framework 'express'
+// importando el framework 'express'
 import express from 'express'
 
 // creando instancia de la aplicacion 'express'
 const app = express()
+
+//este proceso convierte los datos 'json' en un objeto JavaScript accesible
+app.use(express.json())// 'middleware' es un intermediario que le dice a la aplicacion en que formato debe responder
 
 // creando puerto 3000
 const port = 3000
@@ -19,6 +22,18 @@ let usuarios = [{
 // metodo GET
 app.get('/usuarios' , (req,res) =>{
     res.json(usuarios)
+})
+
+// metodo POST
+app.post('/usuarios' , (req,res) => {
+    console.log("Se ha creado el usuario:",req.body)  //Muestra en la consola el cuerpo de la solicitud
+
+    const newUser = {...req.body, id: usuarios.length +1 } // 1. Crear un nuevo usuario con los datos recibidos en req.body, y añadirle un id que es la longitud actual del array usuario + 1.
+
+    usuarios.push(newUser) // 2. Agregar el nuevo usuario al array usuario.
+
+    res.send('Se ha creado un usuario') // 3. Enviar una respuesta al cliente indicando que el usuario ha sido creado.
+    
 })
 
 // haciendo que el servidor este a la escucha en el puerto 3000
